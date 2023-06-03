40b67fcd5aa4548e2b7cd7a63c67110f8b81c979
Souvik Datta <dattasouvik1997@gmail.com>
2023-06-03 16:07:58 +0530

    Added functionality moving to next player after each move

--- src/app/game/game.service.ts
+++ src/app/game/game.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
+import { Player } from '../models/player.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
+  players: Player[] = [
+    { playerId: '#ALCX145', userName: 'Alice' },
+    { playerId: '#BOBA214', userName: 'Bob' },
+    { playerId: '#RCHD754', userName: 'Rich' },
+    { playerId: '#MICS154', userName: 'Michael' },
+    { playerId: '#PAMH754', userName: 'Pamela' },
+  ];
+
   private Enter: string = '+';
   private Cursor: string = '_';
 
   private LetterSubject: Subject<string> = new Subject();
+  private PlayerRotationSubject: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   constructor() { }
 
@@ -22,8 +32,38 @@ export class GameService {
   get letterSubject() {
     return this.LetterSubject.asObservable();
   }
+  get playerRotationSubject() {
+    return this.PlayerRotationSubject.asObservable();
+  }
 
   public nextLetter(letter: string) {
     this.LetterSubject.next(letter);
+  }
+
+  public nextPlayers(isPlayersRotated: boolean) {
+    this.PlayerRotationSubject.next(isPlayersRotated);
+  }
+
+  public rotatePlayers(reverse = false) {
+    // Remove first element from array and store in variable
+    // If first element is not undefined, push it to the end of array
+    if (reverse) {
+      const lastElement = this.players.shift();
+
+      if (lastElement) {
+        this.players.push(lastElement);
+        this.nextPlayers(true);
+      }
+    }
+
+    // Remove last element from array and store in variable
+    // If last element is not undefined, push it to the front of array
+    else {
+      const firstElement = this.players.pop();
+      if (firstElement) {
+        this.players.unshift(firstElement);
+        this.nextPlayers(true);
+      }
+    }
   }
 }
